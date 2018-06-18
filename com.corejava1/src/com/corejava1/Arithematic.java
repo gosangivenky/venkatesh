4c0fca19dd03df735a0999fa9ff67d95773feb14
luckyvenky <luckyvenky@192.168.0.9>
2018-06-18 18:08:45 +0530

    Added variables and sum method

--- com.corejava1/src/com/corejava1/Arithematic.java
+++ com.corejava1/src/com/corejava1/Arithematic.java
@@ -1,5 +1,15 @@
 package com.corejava1;
 
-public class Arithematic {
-
+public class Arithematic 
+{
+	static int a;
+	static int b;
+	static int c;
+	public static void sum()
+	{
+		a=34;
+		b= 67;
+		c  = a+b;
+		System.out.println("sum "+c);
+	}
 }
